7b3e7f0ae7b36c68c42d61233dc1e3802ce6a861
timsuchanek <tim.suchanek@gmail.com>
2019-10-17 18:22:58 +0200

    try if postinstall is executed

--- project/node_modules/whipe-me/setup.js
+++ project/node_modules/whipe-me/setup.js
@@ -3,9 +3,14 @@ const path = require('path')
 
 const enginePath = path.join(__dirname, 'some-engine')
 
-if (!fs.existsSync(enginePath)) {
-  fs.writeFileSync(enginePath, 'wow there is some tension around this engine')
-  console.log(`just wrote a rhyme to ${enginePath}`)
-} else {
-  console.log(`${enginePath} is already here!`)
-}
+const n = (Math.random() * 1000) | 0
+fs.writeFileSync(
+  enginePath,
+  `wow there is some tension around this engine  ${n}`,
+)
+console.log(`just wrote a rhyme to ${enginePath} with a random number ${n}`)
+
+fs.writeFileSync(
+  path.join(__dirname, '../../lol'),
+  `wow there is some tension around this engine  ${n}`,
+)
